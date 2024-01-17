cea503a4afdc82daaf86f46b77e47c8bff5c3c13
Greyson <greysonhansen21@augustana.edu>
2024-01-17 15:39:52 -0600

    fixed bug where skeleton won't follow player

--- final_project/scripts/skeleton.gd
+++ final_project/scripts/skeleton.gd
@@ -5,6 +5,7 @@ class_name Skeleton
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
+	
 	move_and_slide()
 	animated_sprite.play("walk_down")
 	print("velocity " + str(velocity))
--- final_project/scripts/states/enemy_follow.gd
+++ final_project/scripts/states/enemy_follow.gd
@@ -3,6 +3,7 @@ class_name EnemyFollow
 
 @export var enemy: CharacterBody2D
 @export var move_speed := 40.0
+var new_velocity
 var player: CharacterBody2D
 
 func enter():
@@ -12,7 +13,10 @@ func physics_update(delta: float):
 	var direction = player.global_position - enemy.global_position
 	if direction.length() > 0:
 		print("direction " +str(direction.normalized() * move_speed))
-		enemy.velocity = direction.normalized() * move_speed
+		new_velocity = direction.normalized() * move_speed
 	else:
 		enemy.velocity = Vector2()
 
+func _physics_process(delta):
+	if enemy:
+		enemy.velocity = new_velocity
