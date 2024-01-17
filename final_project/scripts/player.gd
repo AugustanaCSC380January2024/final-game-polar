33c95d466e3643e61698310f9b799b023b2c31d6
Greyson <greysonhansen21@augustana.edu>
2024-01-17 14:08:39 -0600

    Added state machine to skeleton and idle state where it wanders around randomly. I have started adding a following state

--- final_project/scripts/player.gd
+++ final_project/scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @onready var animated_sprite = $AnimatedSprite2D
 @export var player_speed = 100
 var direction = "down"
+var health = 100
 
 func _physics_process(delta):
 	velocity = Vector2(0,0)
@@ -18,16 +19,18 @@ func _physics_process(delta):
 	if Input.is_action_pressed("walk_right"):
 		velocity.x = player_speed
 		direction = "right"
+	if Input.is_action_just_pressed("attack"):
+		animated_sprite.play("attack")
 	move_and_slide()
 	update_animations(direction)
 
 func update_animations(direction):
-	
-	if velocity.x == 0 && velocity.y == 0:
-		animated_sprite.play("idle_" + direction)
-	else:
-		animated_sprite.play("walk_" + direction)
-		
+	if animated_sprite :
+		if velocity.x == 0 && velocity.y == 0:
+			animated_sprite.play("idle_" + direction)
+		else:
+			animated_sprite.play("walk_" + direction)
+			
 func _input(event):
 	if event.is_action_pressed("attack"):
 		print("attack")
--- final_project/scripts/skeleton.gd
+++ final_project/scripts/skeleton.gd
@@ -1,2 +1,9 @@
 extends CharacterBody2D
 
+class_name Skeleton
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	move_and_slide()
+	animated_sprite.play("walk_down")
