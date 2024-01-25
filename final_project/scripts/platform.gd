c5ce7a7ac6fb6619272c4ad5a51a58dc295b707f
Greyson <greysonhansen21@augustana.edu>
2024-01-24 21:31:56 -0600

    Added platform to push the box onto. When the box is on, it sends a signal. Started working on a door to open when the platform is pushed

---
+++ final_project/scripts/platform.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+signal pressed
+signal unpressed
+
+
+
+func _on_body_entered(body):
+	if !body is PushObject:
+		return
+	animated_sprite.play("pressed")
+	pressed.emit()
+
+
+func _on_body_exited(body):
+	if !body is PushObject:
+		return
+	animated_sprite.play("unpressed")
+	unpressed.emit()
